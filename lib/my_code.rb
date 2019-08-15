9be7ac9469f3de09969e04ecfb5cc567ad4e2f71
Laura Nickerson <laura.nickerson@gmail.com>
2019-08-15 17:25:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,4 @@
 # Your Code Here
-def my_own_map(source_array, block)
-  array = []
-  i = 0
-  while i < source_array.length do
-    array.push(source_array[i]) #unique
-    i += 1
-  end
-  return array
-end
-
 def map(source_array)
   array = []
   i = 0
